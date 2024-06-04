64f2ced6a80d5ae789411a32707372643c76f147
chof2418 <157701949+chof2418@users.noreply.github.com>
2024-06-04 21:02:31 +0300

    Add files via upload
    add include,
    task

--- oflow_feature_extraction_tb.sv
+++ oflow_feature_extraction_tb.sv
@@ -5,155 +5,90 @@
  * Creation date : Jan 19, 2024
  * Description   :
  *------------------------------------------------------------------------------*/
+`include "/users/epchof/Project/design/work/include_files/oflow_feature_extraction_define.sv"
 
-module oflow_feature_extraction_tb #() ();
+module oflow_feature_extraction_tb #();
+
 
 // -----------------------------------------------------------       
 //                  registers & wires
 // -----------------------------------------------------------  
-	
-	
-	logic clk;
-	logic reset_N;
-	logic [99:0] bbox;
-	logic fe_enable;
-	
-	logic [21:0] cm_concate;
-	logic [43:0] position_concate;
-	logic [10:0] width;
-	logic [10:0] height;
-	logic [23:0] color1;
-	logic [23:0] color2;
-	logic [7:0] d_history; 
-
-   
+  // Testbench signals
+  logic clk;
+  logic reset_N;
+  logic [`BBOX_VECTOR_SIZE-1:0] bbox;
+  logic fe_enable;
   
-   
+  // Outputs
+  logic [`CM_CONCATE_LEN-1:0] cm_concate;
+  logic [`POSITION_CONCATE_LEN-1:0] position_concate;
+  logic [`WIDTH_LEN-1:0] width;
+  logic [`HEIGHT_LEN-1:0] height;
+  logic [`COLOR_LEN-1:0] color1;
+  logic [`COLOR_LEN-1:0] color2;
+  logic [`D_HISTORY_LEN-1:0] d_history;
 
-// ----------------------------------------------------------------------
-//                   Instantiation
-// ----------------------------------------------------------------------
+  // Instantiate the module under test (MUT)
+  oflow_features_extraction  oflow_features_extraction_mut (
+	.clk(clk),
+	.reset_N(reset_N),
+	.bbox(bbox),
+	.fe_enable(fe_enable),
+	.cm_concate(cm_concate),
+	.position_concate(position_concate),
+	.width(width),
+	.height(height),
+	.color1(color1),
+	.color2(color2),
+	.d_history(d_history)
+  );
+//11114523_1100101010_3100101_111_112_
 
-	oflow_features_extraction  oflow_features_extraction(   .clk	(clk),
-		   .reset_N		(reset_N),
-		   
-		   .bbox (bbox),
-		   .fe_enable (fe_enable),
-		   
-	  .cm_concate (cm_concate),
-	  .position_concate  (position_concate),
-	  .width (width),
-	  .height (height),
-	  .color1 (color1),
-	  .color2(color2),
-	  .d_history (d_history)
-		 
-		   
-	  );
-
-
-// ----------------------------------------------------------------------
-//                   Test Pattern
-// ----------------------------------------------------------------------
-
-
-initial 
-begin
-   initiate_all;                                 // Initiates all input signals to '0' and open necessary files
-	 
-   #50
-  
- 	bbox = 100'b00111110100_00011111010_00000010100_00000011110_000000000000000100000000_000000000000000100000000_00001000;
-  	#20
+task create_bbox( input logic [`CM_LEN-1:0] x, input logic [`CM_LEN-1:0] y,input logic [`WIDTH_LEN-1:0] width,input logic [`HEIGHT_LEN-1:0] height,input logic [`COLOR_LEN-1:0] color1,input logic [`COLOR_LEN-1:0] color2,input logic [`D_HISTORY_LEN-1:0] d_history);
+	bbox = {x,y,width,height,color1,color2,d_history};
 	fe_enable = 1'b1;
-   #100 $finish; 
-//   #100000  $finish;
-   
-end
-   
+	@(posedge clk);
+endtask
 
-
-
-// ----------------------------------------------------------------------
-//                   Clock generator  (Duty cycle 5ns)
-// ----------------------------------------------------------------------
-
-   
- always begin
-	#2.5 clk = ~clk;
-	
+  // Clock generation
+  initial begin
+	clk = 0;
+	forever #5 clk = ~clk; // Generate a clock with a period of 10ns
   end
- /*always begin
-	 #2.5 apb_oflow_pclk =~ apb_oflow_pclk;
- end */
-// ----------------------------------------------------------------------
-//                   Tasks
-// ----------------------------------------------------------------------
 
- 
- task initiate_all;        // sets all oflow inputs to '0'.
-	  begin
-		clk = 1'b0;  
-		bbox = 100'd0;		
-	  
-		 #10 reset_N = 1'b1;     // Disable Reset signal.	 
-	  end
- endtask
+  // Test sequence
+  initial begin
+	// Initialize inputs
+	reset_N = 1;
+	bbox = 0;
+	fe_enable = 0;
 
+	// Reset the module
+	#10;
+	reset_N = 0;
 
+	// Drive some test values
+	#10;
+	 create_bbox(50,10,10,100,50,60,6 );
+	 $display("cm_concate: $d,position_concate %d,width: %d ,height: %d,color1: %d ,color2: %d , d_history: %d",cm_concate,position_concate,width,height,color1,color2,d_history);
 
-/*
- task apb_write(input logic [11:0]  addr,input logic [31:0] data);
-	begin
-		
-		// ---------------T1------------
-		@(posedge oflow_clk); 
-		#1
-		apb_oflow_penable = 1'b0;	
-		apb_oflow_pwrite = 1'b1;
-		apb_oflow_psel = 1'b1;	
-		apb_oflow_address = addr;		
-		apb_oflow_pwdata = data;
-		// ---------------T2------------
-		@(posedge oflow_clk);  
-		#1
-		apb_oflow_penable = 1'b1;	
-		// ---------------T3------------
-		 @(posedge oflow_clk);
-		 #1
-		  //apb_oflow_pwrite    = 1'b0;
-		 apb_oflow_psel 		  = 1'b1;
-		 //apb_oflow_address  = 13'h0;
-		// apb_oflow_pwdata  = 31'h0;
-		 apb_oflow_penable = 1'b0;	
-		 $display("Writing Data \n");
-	end
- endtask	
- 
- task apb_read(input logic [11:0]  addr);
-	 begin
-		 
-		 // ---------------T1------------
-		 @(posedge oflow_clk); 
-		 #1
-		 apb_oflow_penable = 1'b0;			 
-		 apb_oflow_pwrite = 1'b0;			
-		 apb_oflow_psel = 1'b1;
-		 apb_oflow_address = addr;	
-		 // ---------------T2------------
-		 @(posedge oflow_clk);
-		 #1
-		 apb_oflow_penable = 1'b1;		 
-		 // ---------------T3------------
-		 @(posedge oflow_clk);
-		 #1 
-		 //apb_oflow_pwrite    = 1'b0;
-		 apb_oflow_psel 		  = 1'b0;
-		 //apb_oflow_address  = 13'h0;
-		 apb_oflow_penable = 1'b0;	
-		$display("Reading Data \n");
-	 end
-  endtask	
- */
- 
+	// Wait for a few clock cycles
+	#100;
+
+	// Change the input values
+	 create_bbox( 30,70,10,100,50,60,6 );
+
+	// Wait for a few clock cycles
+	#100;
+
+	// Finish the simulation
+	$finish;
+  end
+
+  // Optional: Monitor changes on the outputs
+  initial begin
+	$monitor("Time: %0t, cm_concate: %0h, position_concate: %0h, width: %0d, height: %0d, color1: %0h, color2: %0h, d_history: %0b",
+			 $time, cm_concate, position_concate, width, height, color1, color2, d_history);
+  end
+
 endmodule
