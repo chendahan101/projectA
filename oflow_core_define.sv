1f88dc4712d311d9fe21e838843bafd4b78f41b3
chendahan101 <117090635+chendahan101@users.noreply.github.com>
2024-07-07 19:31:39 +0300

    Update oflow_core_define.sv
    add remainder

--- oflow_core_define.sv
+++ oflow_core_define.sv
@@ -11,3 +11,4 @@
 `define SET_LEN 4 //The maximum sets: 256/PE_NUM=11, need 4 bits: 2^4 = 16 > 11
 `define PE_NUM 24 // There are 24 PEs
 `define DATA_OUT_NUM 4 // Num of the dataouts from the mux
+`define REMAINDER_LEN 2 // remainder=1 or2 or 3
