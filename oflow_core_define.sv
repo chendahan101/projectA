cf23237e532edada162cc742cc96f14b6fda9ff2
chof2418 <157701949+chof2418@users.noreply.github.com>
2024-07-21 18:18:07 +0300

    Update oflow_core_define.sv
    `define ROW_LEN 4 //The maximum rows of each pe mem is 11
    change from 3 to 4 because we r going to have 11 line in score board and not just 6
    eg: 256/24=11

--- oflow_core_define.sv
+++ oflow_core_define.sv
@@ -6,7 +6,7 @@
  * Description   :
  *------------------------------------------------------------------------------*/
 
-`define ROW_LEN 3 //The maximum rows of each pe mem is 6
+`define ROW_LEN 4 //The maximum rows of each pe mem is 11
 `define PE_LEN 5 // There are 24 PEs
 `define SET_LEN 4 //The maximum sets: 256/PE_NUM=11, need 4 bits: 2^4 = 16 > 11
 `define PE_NUM 24 // There are 24 PEs
