87d204dbafa67d44a58df32dac6a5b6ca3aa04bf
chof2418 <157701949+chof2418@users.noreply.github.com>
2024-07-21 21:55:54 +0300

    Add files via upload
    oflow_pe: We added new module and we added done_pe for feature_extraction to implement done_fe = 1 till the next start_fe.
    oflow_feature_extraction: We added done_pe as input, fe_enable switches to start_fe, added done_fe = 1 till the next start _fe.

---
+++ oflow_feature_extraction.sv
@@ -0,0 +1,180 @@
+/*------------------------------------------------------------------------------
+ * File          : oflow_feature_extraction.sv
+ * Project       : RTL
+ * Author        : epchof
+ * Creation date : Jan 15, 2024
+ * Description   :
+ *------------------------------------------------------------------------------*/
+`include "/users/epchof/Project/design/work/include_files/oflow_feature_extraction_define.sv"
+
+module oflow_features_extraction (
+
+	// inputs
+	input logic clk,
+	input logic reset_N,
+	
+	input logic [`BBOX_VECTOR_SIZE-1:0] bbox,
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       nable
+	
+	//outputs to registration
+	output logic [`CM_CONCATE_LEN-1:0] cm_concate,
+	output logic [`POSITION_CONCATE_LEN-1:0] position_concate,
+	output logic [`WIDTH_LEN-1:0] width,
+	output logic [`HEIGHT_LEN-1:0] height,
+	output logic [`COLOR_LEN-1:0] color1,
+	output logic [`COLOR_LEN-1:0] color2,
+	
+	// core_fsm
+	input logic done_pe,
+	input logic start_fe,
+	output logic done_fe
+	
+	);
+
+//-----------------------------------------
+//				Wire
+//-----------------------------------------								
+	logic [`CM_LEN-1:0] x_cm;
+	logic [`CM_LEN-1:0] y_cm;
+	logic [`POSTION_TL_LEN-1:0] position_tl;
+	logic [`POSTION_BR_LEN-1:0] position_br;
+	logic [`WIDTH_LEN-1:0] width_tmp;
+	logic [`HEIGHT_LEN-1:0] height_tmp;
+	logic [`COLOR_LEN-1:0] color1_tmp;
+	logic [`COLOR_LEN-1:0] color2_tmp;
+	
+	
+	logic [`CM_CONCATE_LEN-1:0] cm_concate_tmp;
+	logic [`POSITION_CONCATE_LEN-1:0] position_concate_tmp;
+	
+	
+	logic done;
+	
+//-----------------------------------------
+//				out
+//-----------------------------------------
+
+
+	
+//-----------------------------------------
+//				comb_logic
+//-----------------------------------------
+
+	assign width_tmp = bbox[`WIDTH_MSB_IN_BBOX-1:`WIDTH_MSB_IN_BBOX-`WIDTH_LEN];
+	assign height_tmp = bbox[`HEIGHT_MSB_IN_BBOX-1:`HEIGHT_MSB_IN_BBOX-`HEIGHT_LEN];
+	
+	always_comb 
+	begin
+	 position_tl = bbox[`BBOX_VECTOR_SIZE-1:`BBOX_VECTOR_SIZE-`POSTION_TL_LEN];
+	 position_br = {position_tl[`POSTION_TL_LEN-1:`POSTION_TL_LEN-`CM_LEN] + width_tmp, position_tl[`POSTION_TL_LEN-`CM_LEN-1:0] + height_tmp};
+	 x_cm = position_br[`POSTION_BR_LEN-1:`POSTION_BR_LEN-`CM_LEN] >> 1;
+	 y_cm = position_br[`POSTION_BR_LEN-`CM_LEN-1:0] >> 1;
+	 position_concate_tmp = {position_tl, position_br} ;
+	 cm_concate_tmp = {x_cm, y_cm};
+	end	
+	
+	assign color1_tmp = bbox[`COLOR1_MSB_IN_BBOX-1:`COLOR1_MSB_IN_BBOX-`COLOR_LEN];
+	assign color2_tmp = bbox[`COLOR2_MSB_IN_BBOX-1:`COLOR2_MSB_IN_BBOX-`COLOR_LEN];
+	
+
+
+
+
+//-----------------------------------------
+//        instantiation
+//-----------------------------------------
+
+	
+//-----------------------------------------
+//				FF
+//-----------------------------------------
+	
+
+
+	always_ff @(posedge clk or negedge reset_N)   
+	begin    
+		if (!reset_N)  cm_concate <= #1 22'd0;	
+		else if(start_fe)  cm_concate <= #1 cm_concate_tmp;	 
+	end  
+
+	always_ff @(posedge clk or negedge reset_N)   
+	begin    
+		if (!reset_N) position_concate <= #1 44'd0;	
+		else if(start_fe)  position_concate <= #1 position_concate_tmp;
+	end  
+   
+	always_ff @(posedge clk or negedge reset_N)   
+	begin    
+		if (!reset_N) width <= #1 8'd0;	
+		else if(start_fe)	width <= #1 width_tmp;	 
+	end  
+   
+	always_ff @(posedge clk or negedge reset_N)   
+	begin    
+		if (!reset_N) height <= #1 8'd0;	
+		else if(start_fe) height <= #1 height_tmp;
+	end 
+
+	always_ff @(posedge clk or negedge reset_N)   
+	begin 
+		if (!reset_N) color1 <= #1 24'd0;	
+		else if(start_fe) color1 <= #1 color1_tmp;
+	end 
+	
+	always_ff @(posedge clk or negedge reset_N)   
+	begin   
+		if (!reset_N) color2 <= #1 24'd0;	
+		else if(start_fe) color2 <= #1 color2_tmp; 
+	end 	
+   
+   //
+  
+  
+typedef enum {idle_st,fe_st, wait_st} sm_type; //
+sm_type next_state;
+
+
+
+
+// -----------------------------------------------------------       
+//                FSM synchronous procedural block.	
+// -----------------------------------------------------------
+	always_ff @(posedge clk or negedge reset_N) begin
+		if (!reset_N) current_state <= #1 idle_st;
+		else current_state <= #1 next_state;
+	
+	end
+
+ // -----------------------------------------------------------       
+ //						FSM – Async Logic
+ // -----------------------------------------------------------	
+ always_comb begin
+	 next_state = current_state;
+	 done_fe = 0; 
+
+	 case (current_state)
+		 idle_st: begin
+			 //start_read = start_read_mem_for_first_set  ? 1: 0;	
+			 next_state = start_fe  ? fe_st: idle_st;	 
+		 end
+		 
+		 
+		 fe_st: begin 
+			 if( !done_pe )	next_state = wait_st;
+			 else next_state = idle_st;
+		 end
+		 
+ 
+		wait_st: begin 
+			done_fe = 1'b1;
+			 if (start_fe)  next_state = fe_st;
+		 end
+		 
+	 endcase
+ end
+	
+	
+	
+
+
+endmodule  // 
---
+++ oflow_pe.sv
@@ -0,0 +1,139 @@
+/*------------------------------------------------------------------------------
+ * File          : oflow_PE.sv
+ * Project       : RTL
+ * Author        : epchof
+ * Creation date : Jan 13, 2024
+ * Description   :	
+ *------------------------------------------------------------------------------*/
+`include "/users/epchof/Project/design/work/include_files/oflow_similarity_metric_define.sv"
+`include "/users/epchof/Project/design/work/include_files/oflow_feature_extraction_define.sv"
+`include "/users/epchof/Project/design/work/include_files/oflow_core_define.sv"
+`include "/users/epchof/Project/design/work/include_files/oflow_MEM_buffer_define.sv"
+
+module  oflow_pe( 
+			input logic clk,
+			input logic reset_N	,
+			
+			
+			// reg_file
+			input logic [`WEIGHT_LEN-1:0] iou_weight,
+			input logic [`WEIGHT_LEN-1:0] w_weight,
+			input logic [`WEIGHT_LEN-1:0] h_weight,
+			input logic [`WEIGHT_LEN-1:0] color1_weight,
+			input logic [`WEIGHT_LEN-1:0] color2_weight,
+			input logic [`WEIGHT_LEN-1:0] dhistory_weight,
+			
+
+			// dma
+			input logic [`BBOX_VECTOR_SIZE-1:0] bboxes_from_dma,
+			
+			// core_fsm
+			input logic start_fe,
+			input logic start_registration,
+			input logic done_pe,
+			output logic done_fe,
+			output logic done_registration, 
+			output logic done_similarity_metric_i, // the similarity_metric will update this to AND of similarity_metric_0 & similarity_metric_1 in the registration
+			
+
+			// interface between buffer&pe
+			input logic [`DATA_TO_PE_WIDTH -1:0] data_to_pe_0,// we will change the d_history_field
+			input logic [`DATA_TO_PE_WIDTH -1:0] data_to_pe_1,// we will change the d_history_field
+			input logic [`ROW_LEN-1:0] row_sel_to_pe,
+			output logic [`FEATURE_OF_PREV_LEN-1:0] data_out_pe [`PE_NUM],
+			
+			// conflict_resolve
+			
+			
+			
+			);
+			
+// -----------------------------------------------------------       
+//                  logicisters & Wires
+// -----------------------------------------------------------  
+
+	
+	// for registration
+	 logic [`CM_CONCATE_LEN-1:0] cm_concate;
+	 logic [`POSITION_CONCATE_LEN-1:0] position_concate;
+	 logic [`WIDTH_LEN-1:0] width;
+	 logic [`HEIGHT_LEN-1:0] height;
+	 logic [`COLOR_LEN-1:0] color1;
+	 logic [`COLOR_LEN-1:0] color2;
+	// logic [`D_HISTORY_LEN-1:0] d_history; 
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+// -----------------------------------------------------------       
+//				Instantiation
+// -----------------------------------------------------------  
+oflow_feature_extraction oflow_feature_extraction(
+		
+	.clk (clk),
+	.reset_N (reset_N),
+	
+	.done_pe(done_pe),
+	.start_fe (start_fe), // feature_extraction enable 
+	.done_fe   (done_fe),
+	
+	// dma
+	.bbox (bboxes_from_dma),
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     nable
+	
+	// outputs to registration
+	.cm_concate(cm_concate),
+	.position_concate(position_concate),
+	.width(width),
+	.height(height),
+	.color1(color1),
+	.color2(color2),
+	//.d_history(d_history)
+	);
+
+
+oflow_registration oflow_registration( 
+			.clk(clk),
+			.reset_N(reset_N), 
+			
+		
+			// reg_file
+			.iou_weight(iou_weight),
+			.w_weight(w_weight),
+			.h_weight(h_weight),
+			.color1_weight(color1_weight),
+			.color2_weight(color2_weight),
+			.dhistory_weight(dhistory_weight),
+			
+			// core_fsm
+			.start_registration(start_registration),
+			.done_registration(done_registration), 
+			.done_similarity_metric_i(done_similarity_metric_i), // the similarity_metric will update this to AND of similarity_metric_0 & similarity_metric_1 in the registration
+			
+
+			// interface between buffer&pe
+			.data_to_similarity_metric_0(data_to_pe_0),// we will change the d_history_field 
+			.data_to_similarity_metric_1(data_to_pe_1),// we will change the d_history_field
+			.row_sel_to_pe(row_sel_to_pe),
+			.data_out_from_scoreboard(data_out_pe) // for write to mem the features
+			
+			// inputs from feature_extraction
+			.cm_concate(cm_concate),
+			.position_concate(position_concate),
+			.width(width),
+			.height(height),
+			.color1(color1),
+			.color2(color2),
+			//.d_history(d_history) 
+			
+			);
+
+/* oflow_feature_extraction();    */
+
+endmodule
