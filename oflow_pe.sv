0f90d9a65b828ad6a0725ba28386fcd38bc77929
chof2418 <157701949+chof2418@users.noreply.github.com>
2024-07-21 15:57:36 +0300

    Add files via upload
    new module of PE

---
+++ oflow_pe.sv
@@ -0,0 +1,45 @@
+/*------------------------------------------------------------------------------
+ * File          : oflow_PE.sv
+ * Project       : RTL
+ * Author        : epchof
+ * Creation date : Jan 13, 2024
+ * Description   :	
+ *------------------------------------------------------------------------------*/
+
+module  oflow_PE( 
+			input logic clk,
+			input logic reset_N	,
+			input logic [111:0] data_in,
+			input logic wr,
+			input logic [7:0] addr,
+			input logic  EN,
+			
+			
+			output logic [111:0] data_out
+			);
+			
+// -----------------------------------------------------------       
+//                  logicisters & Wires
+// -----------------------------------------------------------  
+
+	//next 
+
+
+// -----------------------------------------------------------       
+//				Instanciation
+// -----------------------------------------------------------  
+oflow_registration oflow_registration( 
+			.clk(clk),
+			.reset_N(reset_N)	,
+			.data_in(data_in),
+			.wr(wr),
+			.addr(addr),
+			.EN(EN),// 
+			
+			
+			.data_out(data_out)
+					);
+
+/* oflow_feature_extraction();    */
+
+endmodule
