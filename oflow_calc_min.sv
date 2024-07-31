0513f3028b460dd33e460d832dba73be0f8c6ad0
chof2418 <157701949+chof2418@users.noreply.github.com>
2024-07-31 13:00:39 +0300

    Add files via upload
    add nested if in calc min

--- oflow_calc_min.sv
+++ oflow_calc_min.sv
@@ -35,22 +35,133 @@ module oflow_calc_min #() (
 	output logic [`SCORE_LEN-1:0] min_score_0, // min_score_0
 	output logic [`ID_LEN-1:0] min_id_0, // id_0 of min score_0 
 	output logic [`SCORE_LEN-1:0] min_score_1, // min_score_1
-	output logic [`ID_LEN-1:0] min_id_1, // id_1 of min score_1 
-
+	output logic [`ID_LEN-1:0] min_id_1 // id_1 of min score_1 
+);
 
 // -----------------------------------------------------------       
 //              Logics
 // -----------------------------------------------------------  
 
-logic [`SCORE_LEN-1:0] min_score_0_reg, // min_score_0
-logic [`ID_LEN-1:0] min_id_0_reg, // id_0 of min score_0 
-logic [`SCORE_LEN-1:0] min_score_1_reg, // min_score_1 (will be the maximum between min_score_0_reg and min_score_1_reg)
-logic [`ID_LEN-1:0] min_id_1_reg, // id_1 of min score_1 (will be the id of the maximum between min_score_0_reg and min_score_1_reg)
+logic [`SCORE_LEN-1:0] min_score_0_reg; // min_score_0
+logic [`ID_LEN-1:0] min_id_0_reg; // id_0 of min score_0 
+logic [`SCORE_LEN-1:0] min_score_1_reg; // min_score_1 (will be the maximum between min_score_0_reg and min_score_1_reg)
+logic [`ID_LEN-1:0] min_id_1_reg; // id_1 of min score_1 (will be the id of the maximum between min_score_0_reg and min_score_1_reg)
 
 typedef enum {idle_st, calc_min_st} sm_type; 
 sm_type current_state;
 sm_type next_state;
 
+
+// -----------------------------------------------------------       
+//                FSM synchronous procedural block.	
+// -----------------------------------------------------------
+	always_ff @(posedge clk or negedge reset_N) begin
+		if (!reset_N) current_state <= #1 idle_st;
+		else current_state <= #1 next_state;
+	
+	end
+//--------------------min_score_0_reg---------------------------------	
+	 always_ff @(posedge clk or negedge reset_N) begin
+		if (!reset_N || start_score_calc) min_score_0_reg <= #1 {32{1'b1}} ;
+		else if(put_0_in_0 )	min_score_0_reg <= #1 score_0;
+		else if(put_1_in_0 ) <= #1 min_score_0_reg <= #1 score_1;
+	 end
+//--------------------min_score_1_reg---------------------------------	
+	 always_ff @(posedge clk or negedge reset_N) begin
+		if (!reset_N || start_score_calc) min_score_0_reg <= #1 {32{1'b1}} ;
+		else if(put_0_in_1 )	min_score_1_reg <= #1 score_0;
+		else if(put_1_in_1 ) <= #1 min_score_1_reg <= #1 score_1;
+		else if(put_min_0_in_1 ) <= #1 min_score_1_reg <= #1 min_score_0_reg;
+		
+	 end
+	 
+	//--------------------min_id_0_reg---------------------------------	
+	 always_ff @(posedge clk or negedge reset_N) begin
+		if (!reset_N || start_score_calc) min_id_0_reg <= #1 0 ;
+		else if(put_0_in_0 )	min_id_0_reg <= #1 id_0;
+		else if(put_1_in_0 ) <= #1 min_id_0_reg <= #1 id_1;
+	 end
+//--------------------min_id_1_reg---------------------------------	
+	 always_ff @(posedge clk or negedge reset_N) begin
+		if (!reset_N || start_score_calc) min_id_1_reg <= #1 0 ;
+		else if(put_0_in_1 )	min_id_1_reg <= #1 id_0;
+		else if(put_1_in_1 ) <= #1 min_id_1_reg <= #1 id_1;
+		else if(put_min_0_in_1 ) <= #1 min_id_1_reg <= #1 min_id_0_reg;
+		
+	 end
+	 
+	  
+	 
+	 
+ // -----------------------------------------------------------       
+ //						FSM – Async Logic
+ // -----------------------------------------------------------	
+ always_comb begin
+	 next_state = current_state;
+	 
+	 done_calc_min = 0;
+	 put_0_in_1 = 0;	
+	 put_1_in_1 =0 ;
+	 put_0_in_0 = 0;
+	 put_1_in_0 = 0;
+	 put_min_0_in_1 = 0;
+	 
+	 case (current_state)
+		idle_st: begin
+			 next_state = start_calc_min ? calc_min_st: idle_st;	
+			 
+		 end
+		 
+		 calc_min_st: begin
+
+			if (not (score_0 >= min_score_1_reg and score_1 >= min_score_1_reg ) ) begin 
+				if  (min_score_0_reg=< score_0 < min_score_1_reg and score_1 >= min_score_1_reg )  put_0_in_1=1'b1 
+				else if  (min_score_0_reg=< score_1 < min_score_1_reg and score_0 >= min_score_1_reg)  put_1_in_1=1'b1 
+				else if  (min_score_0_reg=< score_0 < min_score_1_reg and min_score_0_reg=< score_1 < min_score_1_reg) begin 
+					if (score_0<score_1)  put_0_in_1=1'b1  
+					else   put_1_in_1=1'b1  
+				end 
+				else if  (score_0 < min_score_0_reg and min_score_1_reg=< score_1 ) begin
+					put_0_in_0 = 1'b1
+					put_min_0_in_1 = 1'b1
+				end
+				else if  (score_1 < min_score_0_reg and min_score_1_reg=< score_0 ) begin
+					put_1_in_0 = 1'b1
+					put_min_0_in_1 = 1'b1
+				end
+				else if  (score_0 < min_score_0_reg and min_score_0_reg=< score_1 < min_score_1_reg) begin
+					put_0_in_0 = 1'b1
+					put_min_0_in_1 = 1'b1
+				end
+				else if  (score_1 < min_score_0_reg and min_score_0_reg=< score_0 < min_score_1_reg) begin
+					put_1_in_0 = 1'b1
+					put_min_0_in_1 = 1'b1
+				end
+				else if  (score_1 < min_score_0_reg and score_0 < min_score_0_reg) begin
+					if (score_0<score_1) begin  put_0_in_0=1'b1; put_1_in_1=1'b1 end 
+					else begin   put_0_in_1=1'b1; put_1_in_0=1'b1 end 
+				end
+			end 
+			
+			
+			
+			
+			next_st = idle_st;
+			done_calc_min = 1;
+		 end
+		 
+		 
+	 endcase
+ end
+	  
+	assign min_score_0 = min_score_0_reg;
+	assign min_score_1 = min_score_1_reg;
+	assign min_id_0 = min_id_0_reg;
+	assign min_id_1 = min_id_1_reg ;
+
+	 
+	 
+endmodule
 
 
 
